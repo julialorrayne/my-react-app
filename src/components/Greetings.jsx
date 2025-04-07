40ee73c54be46208a6ed754a64fdd91285df1f6e
Julia <lorrayne@usf.edu>
2025-04-06 22:50:53 -0400

    content on website

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function Greetings({name}) {
   return (
     <div>
-      <h3>Helo,{name}!</h3>
-      <p>"Please take a look at our products below!"</p>
+        <h2>Hello from the Greeting Component!</h2>
+        <p>This is your first custom component in React.</p>
+        <h3>Helo,{name}!</h3>
+        <p>"Please take a look at our products below!"</p>
     </div>
   );
 }
